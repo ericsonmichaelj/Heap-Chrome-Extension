d7c8f45f995cc5f0bdec15b4b26b1ef9f82798da
Michael Ericson <ericsonm@Michaels-MBP.home>
2018-05-27 19:35:53 -0400

    Added ability to toggle image and send messages to tab

---
+++ dist/background.js
@@ -0,0 +1,40 @@
+
+var debug = true;
+
+chrome.browserAction.onClicked.addListener(
+    function(tab) {
+        debug = !debug;
+        var offIcon = {
+            "16": "../resources/icon_off.png",
+            "32": "../resources/icon_off.png",
+            "48": "../resources/icon_off.png",
+            "128": "../resources/icon_off.png"
+        }
+        var onIcon = {
+            "16": "../resources/icon.png",
+            "32": "../resources/icon.png",
+            "48": "../resources/icon.png",
+            "128": "../resources/icon.png"            
+        }
+        chrome.browserAction.setTitle({
+            title: debug ? 'Heap Analytics debugger is ON' : 'Heap Analytics debugger is OFF'
+        });
+
+        chrome.browserAction.setIcon({
+            path: debug ? '/resources/icon.png': '/resources/icon_off.png'
+        });
+    }
+);
+
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        if (debug) {
+            chrome.tabs.query({active: true, currentWindow: true},function(tabs) {
+                console.log('this is details', details)
+                chrome.tabs.sendMessage(tabs[0].id, details);
+            });
+        }
+    },
+    { urls: ["https://heapanalytics.com/*"] }, ['requestBody', 'blocking']
+);
---
+++ dist/src/analytics.js
@@ -0,0 +1,18 @@
+class HeapAnalyticsDisplayer {
+  constructor(sender) {
+
+  }
+  _parseQueryParamaters() {
+
+  }
+}
+
+
+
+
+
+chrome.extension.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    return;
+  }
+);
