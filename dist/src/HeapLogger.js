96743051c4d0a3b12f1b6a3ac2ac40401ddbdabe
Michael Ericson <ericsonm@Michaels-MBP.home>
2018-05-28 00:39:48 -0400

    Add logging of events

--- dist/src/HeapLogger.js
+++ dist/src/HeapLogger.js
@@ -3,35 +3,106 @@ const HeapLogger = class {
   constructor(url) {
     this.queryString = url ? queryString.parselUrl(url).query : null;
     this.identity = this.queryString ? this.queryString.i : null;
+    this.userProperties = null;
+    this.events = null;
+
   }
   setUserProperties() {
     if(!this.queryString) return;
     const userProperties = {}
     for (let queryStringKey in this.queryString) {
       if(queryStringKey[0] === '_') {
-        userProperties[queryStringKey.substr(1)] = this.queryString[queryStringKey]
+        userProperties[queryStringKey.substr(1)] = this.queryString[queryStringKey];
       }
     }
-    if (Object.keys(userProperties).length > 0) this.userProperties = userProperties
+    this.userProperties = Object.keys(userProperties).length > 0 ? userProperties: null;
   }
+
+  _isEvent(queryStringKey) {
+    return queryStringKey[0] === 't' && !isNaN(parseInt(queryStringKey.substr(1)));
+  }
+
+  _isCustomEvent(event) {
+    return event !== 'click' && event !== 'submit' && event !== 'change';
+  }
+
+  _isEventProperty(queryStringKey) {
+    return queryStringKey[0] === 'k' && !isNaN(parseInt(queryStringKey.substr(1)));
+  }
+
+  _getEventId(queryStringKey) {
+    return parseInt(queryStringKey.substr(1))
+  }
+
+  setEvents() {
+    if(!this.queryString) return;
+     const events = [];
+     for (let queryStringKey in this.queryString) {
+      if(this._isEvent(queryStringKey) && this._isCustomEvent(this.queryString[queryStringKey])) {
+        const eventId = this._getEventId(queryStringKey);
+        events[eventId] = {name: this.queryString[queryStringKey], properties: {}};
+      }
+    }
+    this.events = events.length>0 ? events : null;
+  }
+
+  setEventsProperties() {
+     for (let queryStringKey in this.queryString) {
+      if(this._isEventProperty(queryStringKey)) {
+        const eventId = this._getEventId(queryStringKey);
+        const eventProperties = this.queryString[queryStringKey];
+        if (Array.isArray(eventProperties)) {
+          for (let i =0;i < eventProperties.length;i+=2) {
+            const key = eventProperties[i];
+            const value = eventProperties[i+1];
+            if(his.events[eventId]) {
+              this.events[eventId].properties[key] = value;
+            }
+          }
+        }
+      }
+    }    
+  }
+
   static init(url) {
     count++
-    const heapLogger = new HeapLogger(url)
-    heapLogger.setUserProperties()
-    heapLogger.print()
-    return heapLogger
+    const heapLogger = new HeapLogger(url);
+    heapLogger.setUserProperties();
+    heapLogger.setEvents()
+    heapLogger.setEventsProperties();
+    // TO DO: Console.log Event and Event Properties. Event Properties are kX.
+    // Events are tX (avoid click and change events) 
+    heapLogger.print();
+    return heapLogger;
+  }
+  _printEvents() {
+    console.group('Event');
+    if(this.events) {
+      for(const event of this.events) {
+        if(event) {
+          if(event.name) console.log('name:', event.name);
+          if(Object.keys(event.properties).length > 0)  {
+            console.log('properties:')
+            console.table(event.properties);
+          }
+        }
+      }
+    }
+    console.groupEnd();
+  }
+  _printUserInfo() {
+    console.group('User Info');
+      console.log('identity:', this.identity);
+      if(this.userProperties) {
+        console.log('User Properties:');
+        console.table(this.userProperties);
+      }
+    console.groupEnd();    
   }
   print() {
     console.group('Heap Analytics Log ' + count);
-      console.group('User Info');
-        console.log('identity:', this.identity);
-        if(this.userProperties) {
-          console.log('User Properties:')
-          console.table(this.userProperties)
-        }
-      console.groupEnd();
-      console.group('Event');
-      console.groupEnd();
+      this._printUserInfo();
+      this._printEvents();
     console.groupEnd();
   }
 }
--- test/spec/index.spec.js
+++ test/spec/index.spec.js
@@ -17,10 +17,24 @@ describe('Heap Logger', () => {
     expect(heapLogger.identity).to.equal('test');
   })
   it('should contain the user properties', () => {
-     const heapLogger = HeapLogger.init('https://example.com?_test=testA&_a=b');
-     console.log(heapLogger.userProperties)
-     expect(Object.keys(heapLogger.userProperties).length).to.equal(2)
-     expect(heapLogger.userProperties.test).to.equal('testA');
-     expect(heapLogger.userProperties.a).to.equal('b');
+    const heapLogger = HeapLogger.init('https://example.com?_test=testA&_a=b');
+    expect(Object.keys(heapLogger.userProperties).length).to.equal(2)
+    expect(heapLogger.userProperties.test).to.equal('testA');
+    expect(heapLogger.userProperties.a).to.equal('b');
   });
+  it('should set event name  from query string', () => {
+    const heapLogger = HeapLogger.init('https://example.com?t0=test&t1=click&t2=foo');
+    expect(heapLogger.events[0].name).to.equal('test');
+    expect(heapLogger.events[1]).to.equal(undefined);
+    expect(heapLogger.events[2].name).to.equal('foo');
+  });
+  it('should not set events if query string does not contain event property', () => {
+    const heapLogger = HeapLogger.init('https://example.com?tl=test');
+    expect(heapLogger.events).to.equal(null);    
+  });
+  it('should have an events properties', () => {
+    const heapLogger = HeapLogger.init('https://example.com?t0=test&k0=foo&k0=bar&k0=hello&k0=world');
+    expect(heapLogger.events[0].properties.foo).to.equal('bar');
+    expect(heapLogger.events[0].properties.hello).to.equal('world');       
+  })
 });
