569c508c0a28137dadb5926529aa0e71c8c66a35
Michael Ericson <ericsonm@Michaels-MacBook-Pro.local>
2018-06-03 22:10:21 -0400

    modify siteConfig.js to use on gitpages

--- docs/website/siteConfig.js
+++ docs/website/siteConfig.js
@@ -14,7 +14,7 @@ const users = [
     caption: 'User1',
     // You will need to prepend the image path with your baseUrl
     // if it is not '/', like: '/test-site/img/docusaurus.svg'.
-    image: '/img/docusaurus.svg',
+    image: '/Heap-Chrome-Extension/img/docusaurus.svg',
     infoLink: 'https://www.facebook.com',
     pinned: true,
   },
@@ -23,15 +23,15 @@ const users = [
 const siteConfig = {
   title: 'Test Site' /* title for your website */,
   tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com' /* your website url */,
-  baseUrl: '/' /* base url for your project */,
+  url: 'https://ericsonmichaelj.github.io' /* your website url */,
+  baseUrl: '/Heap-Chrome-Extension/' /* base url for your project */,
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'Heap-Chrome-Extension',
+  organizationName: 'ericsonmichaelj',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
@@ -88,8 +88,8 @@ const siteConfig = {
   onPageNav: 'separate',
 
   /* Open Graph and Twitter card images */
-  ogImage: 'img/docusaurus.png',
-  twitterImage: 'img/docusaurus.png',
+  ogImage: 'Heap-Chrome-Extension/img/docusaurus.png',
+  twitterImage: 'Heap-Chrome-Extension/img/docusaurus.png',
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
