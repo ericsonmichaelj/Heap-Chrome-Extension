7304deb51337b2d45086f08ee763e378990e9d39
Michael Ericson <michaelericson@Michaels-MacBook-Pro.local>
2024-06-07 14:25:24 -0400

    update heap to latest version

--- dist/src/analytics.js
+++ dist/src/analytics.js
@@ -1,4 +1,4 @@
-chrome.extension.onMessage.addListener(
+chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     const heapLogger = new HeapLogger(request.url);
     heapLogger.run();
